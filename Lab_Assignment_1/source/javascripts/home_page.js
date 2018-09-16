2279e8d8eaa72271fcff3785b8cde5ac27580909
kaphc <kaphc@mail.umkc.edu>
2018-09-16 17:24:35 -0500

    code setup

--- Lab_Assignment_1/source/javascripts/home_page.js
+++ Lab_Assignment_1/source/javascripts/home_page.js
@@ -1,10 +1,13 @@
-angular.module('search-engine', []).controller('search-engine-ctrl', function ($scope, $http) {
+angular.module("search-engine", []).controller("search-engine-ctrl", function ($scope, $http) {
+
     $scope.getResult = function () {
         let search_word = document.getElementById("search-word").value;
-        $http.get('https://kgsearch.googleapis.com/v1/entities:search?query=' + search_word + '&key=AIzaSyDF_fECqSQmSoROurh_GyBTr-CvHN9O7VI&limit=1&indent=True').then(function (response) {
+        let url_part_1 = "https://kgsearch.googleapis.com/v1/entities:search?query=";
+        let url_part_2 = "&key=AIzaSyDF_fECqSQmSoROurh_GyBTr-CvHN9O7VI&limit=1&indent=True";
+        $http.get(url_part_1 + search_word + url_part_2).then(function (response) {
 
-            var information_table = document.getElementById("information-table");
-            var image_table = document.getElementById("image");
+            let information_table = document.getElementById("information-table");
+            let image_table = document.getElementById("image");
 
             for (var information_table_iter = information_table.rows.length - 1; information_table_iter > -1; information_table_iter--) {
                 information_table.deleteRow(information_table_iter);
@@ -20,34 +23,73 @@ angular.module('search-engine', []).controller('search-engine-ctrl', function ($
             let fourth_row = information_table.insertRow(3);
             let fifth_row = information_table.insertRow(4);
             let sixth_row = information_table.insertRow(5);
+            let seventh_row = information_table.insertRow(6);
             let image_row = image.insertRow(0);
 
+            let name, type, description, article, wiki_link, site, license, icon_url;
 
-            let icon_url = '<p align="center"><img src=' + response.data['itemListElement'][0]['result']['image']['contentUrl'] + '></p>';
-            let wiki_link = '<a href="' + response.data['itemListElement'][0]['result']['detailedDescription']['url'] + '">' + response.data['itemListElement'][0]['result']['detailedDescription']['url'] + '</a>';
-            let site = '<a href="' + response.data['itemListElement'][0]['result']['url'] + '">'+response.data['itemListElement'][0]['result']['url']+'</a>';
-            let license = '<a href="' + response.data['itemListElement'][0]['result']['detailedDescription']['license'] + '">'+response.data['itemListElement'][0]['result']['detailedDescription']['license']+'</a>';
+            try {
+                name = response.data["itemListElement"][0]["result"]["name"];
+            } catch (e) {
+                name = "NOT AVAILABLE";
+            }
             first_row.insertCell(0).innerHTML = "Name";
-            first_row.insertCell(1).innerHTML = response.data['itemListElement'][0]['result']['name'];
+            first_row.insertCell(1).innerHTML = name;
 
-            second_row.insertCell(0).innerHTML = "Description";
-            second_row.insertCell(1).innerHTML = response.data['itemListElement'][0]['result']['description'];
+            try {
+                type = response.data["itemListElement"][0]["result"]["@type"];
+            } catch(e) {
+                type = "NOT AVAILABLE";
+            }
+            second_row.insertCell(0).innerHTML = "TYPE";
+            second_row.insertCell(1).innerHTML = type;
 
-            third_row.insertCell(0).innerHTML = "Article";
-            third_row.insertCell(1).innerHTML = response.data['itemListElement'][0]['result']['detailedDescription']['articleBody'];
+            try {
+                description = response.data["itemListElement"][0]["result"]["description"];
+            } catch (e) {
+                description = "NOT AVAILABLE";
+            }
+            third_row.insertCell(0).innerHTML = "Description";
+            third_row.insertCell(1).innerHTML = description;
 
-            fourth_row.insertCell(0).innerHTML = "Wiki Page";
-            fourth_row.insertCell(1).innerHTML = wiki_link;
+            try {
+                article = response.data["itemListElement"][0]["result"]["detailedDescription"]["articleBody"];
+            } catch (e) {
+                article = "NOT AVAILABLE";
+            }
+            fourth_row.insertCell(0).innerHTML = "Article";
+            fourth_row.insertCell(1).innerHTML = article;
 
-            fifth_row.insertCell(0).innerHTML = "URL";
-            fifth_row.insertCell(1).innerHTML = site;
+            try {
+                wiki_link = "<a href=\"" + response.data["itemListElement"][0]["result"]["detailedDescription"]["url"] + "\">" + response.data["itemListElement"][0]["result"]["detailedDescription"]["url"] + "</a>";
+            } catch (e) {
+                wiki_link = "NOT AVAILABLE";
+            }
+            fifth_row.insertCell(0).innerHTML = "Wiki Page";
+            fifth_row.insertCell(1).innerHTML = wiki_link;
 
-            sixth_row.insertCell(0).innerHTML = "License";
-            sixth_row.insertCell(1).innerHTML = license;
+            try {
+                site = "<a href=\"" + response.data["itemListElement"][0]["result"]["url"] + "\">" + response.data["itemListElement"][0]["result"]["url"] + "</a>";
+            } catch (e) {
+                site = "NOT AVAILABLE";
+            }
+            sixth_row.insertCell(0).innerHTML = "Website";
+            sixth_row.insertCell(1).innerHTML = site;
 
+            try {
+                license = "<a href=\"" + response.data["itemListElement"][0]["result"]["detailedDescription"]["license"] + "\">" + response.data["itemListElement"][0]["result"]["detailedDescription"]["license"] + "</a>";
+            } catch (e) {
+                license = "NOT AVAILABLE";
+            }
+            seventh_row.insertCell(0).innerHTML = "License";
+            seventh_row.insertCell(1).innerHTML = license;
 
+            try {
+                icon_url = "<p align=\"center\"><img src=" + response.data["itemListElement"][0]["result"]["image"]["contentUrl"] + "></p>";
+            } catch (e) {
+                icon_url = "NOT AVAILABLE";
+            }
             image_row.insertCell(0).innerHTML = icon_url;
-
         });
     }
 
