0a7fe67218f65c06f6f54795df7cd3013abc1aea
kaphc <kaphc@mail.umkc.edu>
2018-09-18 00:47:15 -0500

    final setup

--- Lab_Assignment_1/source/javascripts/login.js
+++
@@ -1,6 +0,0 @@
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    $(".g-signin2").css("display", "none");
-    $(".data").css("display", "block");
-    $("#email").text(profile.getEmail());
-}
